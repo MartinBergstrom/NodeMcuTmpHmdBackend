aa0a5080e2790f2ea1f3a47f37e42cf65b91ccde
Martin Bergström <emarbe02@gmail.com>
2019-12-02 22:50:49 +0100

    replace with eclipse project

---
+++ src/com/martin/ab/CustomFileWriter.java
@@ -0,0 +1,40 @@
+package com.martin.ab;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CustomFileWriter {
+    private static final int MAX_ALLOWED_LOG_FILE_SIZE_MB = 200;
+    private static int FILE_NBR = 1;
+
+    public void writeLine(String logLine) throws IOException {
+        String filePath = getFileName();
+   
+        File file = new File(filePath);
+        if (!file.createNewFile()) {
+            if (overSizeLimit(file)) {
+                FILE_NBR++;
+                filePath = getFileName();
+                file.createNewFile();
+            }
+        }
+
+        String lineToWrite = logLine + "\n";
+        
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true)); 
+        writer.append(lineToWrite);
+        writer.close();
+    }
+
+    public static String getFileName() {
+        //return System.getProperty("user.dir") + File.separator + "log" + FILE_NBR + ".txt";
+    	return "C:\\Users\\Martin\\Documents\\log" + FILE_NBR + ".txt";
+    }
+
+    private boolean overSizeLimit(File file) {
+        long fileLengthInBytes = file.length();
+        return ( (fileLengthInBytes / 1024) / 1024 ) > MAX_ALLOWED_LOG_FILE_SIZE_MB;
+    }
+}
---
+++ src/com/martin/ab/LogResource.java
@@ -0,0 +1,72 @@
+package com.martin.ab;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/log")
+public class LogResource {
+   private CustomFileWriter myCustomFileWriter = new CustomFileWriter();
+      
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response doGet() throws IOException {
+		String fileName = CustomFileWriter.getFileName();
+
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+	    StringBuilder sb = new StringBuilder();
+		try {
+		    String line = br.readLine();
+
+		    while (line != null) {
+		        sb.append(line);
+		        sb.append(System.lineSeparator());
+		        line = br.readLine();
+		    }
+		} finally {
+		    br.close();
+		}
+        
+        return Response.status(200)
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Credentials", "true")
+        .header("Access-Control-Allow-Methods", "POST, GET")
+        .header("Access-Control-Allow-Headers", "Content-Type")
+        .entity(sb.toString())
+        .build();
+	}
+
+	@Path("new")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response postLog(String logLine) {
+        String finalLogLine = getTimeStamp() + logLine;
+  
+        try {
+        	myCustomFileWriter.writeLine(finalLogLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Response.status(500).entity("Error occurred during writing to file..")
+                    .build();
+        }
+        return Response.status(201)
+                .build();
+	}
+	
+	private String getTimeStamp()
+	{
+		return new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss")
+				.format(new Date());
+	}
+
+}
